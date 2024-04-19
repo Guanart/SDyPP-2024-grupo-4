5331ddb23a91b1097c07d9e5ba93694cb5d877c4
Luklr <lucioreinoso88@gmail.com>
2024-04-19 15:36:53 -0300

    server_v2
    Co-authored-by: Francisco Guerra <FrannGuerra@users.noreply.github.com>
    Co-authored-by: Gonzalo Benito <Guanart@users.noreply.github.com>

--- TP2/H1/server_v2/servidor_v2.py
+++ TP2/H1/server_v2/servidor_v2.py
@@ -4,20 +4,21 @@ import requests
 import time
 
 app = Flask(__name__)
-client_docker = docker.from_env()
+
 
 @app.route("/getRemoteTask", methods=['GET', 'POST'])
 def ejecutarTareaRemota():
+    client_docker = docker.from_env()
     task_params = request.get_json()
     print(task_params)
-    id_container = client_docker.containers.run(task_params['image'], detach=True, ports={'8000/tcp': 8000})
+    id_container = client_docker.containers.run(task_params['image'], detach=True, ports={'5000/tcp': 8022})
     container = client_docker.containers.get(id_container.id)
-
+    time.sleep(3)
     while not container.status == 'running':
         print("Esperando a que el contenedor esté en ejecución...")
         time.sleep(3)  # Esperar 3 segundos antes de volver a verificar
 
-    response = requests.post('http://34.138.195.122:8000/ejecutarTarea', json=task_params)
+    response = requests.post('http://host.docker.internal:8022/ejecutarTarea', json=task_params)
     container.stop()
     container.remove()
     return response.json()
