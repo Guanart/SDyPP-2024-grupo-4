c554446648d8eb3c88704a6e73b6500d88ba7fc6
Luklr <lucioreinoso88@gmail.com>
2024-05-28 15:23:37 -0300

    h3 casi terminado

--- TP4/H3/pipeline-apps.sh
+++ TP4/H3/pipeline-apps.sh
@@ -5,11 +5,3 @@ kubectl apply -f deployments/web-server-deployment.yaml
 kubectl apply -f deployments/unificador-deployment.yaml
 kubectl apply -f deployments/particionador-deployment.yaml
 
-
-crack master fiera, como genero en la nube el unificador
-tengo q romper toda la infra?? ....zzzzzzz
-no
-un nuevo deployment y listo creo
-
-hice kubectl apply -f . y nada
-estoy en ubuntu xddddddddddddddddddd
