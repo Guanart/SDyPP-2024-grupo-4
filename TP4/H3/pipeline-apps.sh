41f2d23cdf18eac427665dbb0d235ed30062e8e6
Francisco Guerra <fran03guerra@gmail.com>
2024-05-31 15:14:48 -0300

    Organizando deployments
    Co-authored-by: Gonzalo Benito <Guanart@users.noreply.github.com>

--- TP4/H3/pipeline-apps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcloud container clusters get-credentials primary --region=us-east1-b
-kubectl apply -f deployments/web-server-deployment.yaml
-kubectl apply -f deployments/unificador-deployment.yaml
-kubectl apply -f deployments/particionador-deployment.yaml
-
--- TP4/H3/pipeline-servicios.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud container clusters get-credentials primary --region=us-east1-b
-kubectl apply -f deployments/rabbitmq-deployment.yaml
-kubectl apply -f deployments/redis-deployment.yaml
